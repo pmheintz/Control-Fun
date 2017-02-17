4d40a33adfca7322face9a398deaa9007874c5ad
Paul Heintz <pheintz@damien>
2017-02-17 16:50:09 -0600

    Initial Commit

---
+++ Control Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by Paul Heintz on 2/17/17.
+//  Copyright © 2017 Paul Heintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
