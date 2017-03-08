660270f5b56e35eae45ed118569d2346099c9eab
Paul Heintz <pmheintz9@gmail.com>
2017-03-07 18:38:44 -0600

    Mid-point

--- Control Fun/ViewController.swift
+++ Control Fun/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func onTapGestureRecognized(_ sender: Any) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
 }
 
